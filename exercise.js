e1d81e0d2b6a97f624a0385585e70166ea1d38e5
brthzz <brthzz@gmail.com>
2019-09-23 13:45:22 +0200

    all basic exercises

--- exercise.js
+++ exercise.js
@@ -2,7 +2,7 @@ function ageCalculator(current,birth) {
 
    var yourAge = current - birth;
    var yourAge2 = yourAge -1;
-
+   document.write("<br>");
    document.write("You are either " + yourAge + " or " + yourAge2)
 }
 
@@ -10,7 +10,7 @@ ageCalculator(2019, 2000)
 
 
 
-// basic ex 03
+// Basic 3 | Age Calculator-improved
 
 
 function ageCalculator(birth) {
@@ -20,7 +20,35 @@ function ageCalculator(birth) {
   	var yourAge = current - birth;
    	var yourAge2 = yourAge -1;
    console.log(year);
+   document.write("<br>");
    document.write("You are either " + yourAge + " or " + yourAge2);
 }
 
 ageCalculator(2000)
+
+// Basic 4 | Degree-Radian Conversion
+
+function convertDegree(degree) {
+	
+	var oneRadian = Math.PI / 180;
+	var result = oneRadian * degree;
+	document.write("<br>");
+	document.write(result);
+}
+
+// Basic 5 | Area and Volume of a box
+
+function getArea(length,width) {
+	 
+	var resultArea = length * width;
+	return resultArea;
+	console.log(resultArea);
+}
+
+var firstResult = resultArea;
+
+function getVolume(depth) {
+
+	var resultVolume = firstResult * depth;
+ 	console.log(resultVolume);
+}
